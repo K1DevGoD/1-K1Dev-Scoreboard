d9c1a9a8acd200e3cbb58af93a594091bc9cd06b
K1Dev <devgodno1@gmail.com>
2022-10-15 13:32:08 +0700

    1-K1Dev-Scoreboard 1.1
    1-K1Dev-Scoreboard 1.1

---
+++ Config.lua
@@ -0,0 +1,12 @@
+--[[
+        
+        💬 Export from K1Dev => discord: https://discord.gg/awayfromus ] 
+        🐌 @Copyright K1Dev
+        ☕ Thanks For Coffee Tips 
+        🧠 Development team => "RDX-Dev"
+--]]
+
+
+Config = {}
+
+Config['ปุ่มเปิด'] = 0x4AF4D473
---
+++ fxmanifest.lua
@@ -0,0 +1,31 @@
+--[[
+        
+        💬 Export from K1Dev => discord: https://discord.gg/awayfromus ] 
+        🐌 @Copyright K1Dev
+        ☕ Thanks For Coffee Tips 
+        🧠 Development team => "RDX-Dev"
+--]]
+
+fx_version 'adamant'
+games {'rdr3'}
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+author 'K1Dev'
+version '1.0'
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'Config.lua',
+	'server.lua'
+}
+
+client_scripts {
+	'Config.lua',
+	'client.lua'
+}
+
+ui_page "html/ui.html"
+
+files {
+	"html/*"
+}
